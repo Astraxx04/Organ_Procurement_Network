fc263f659aa62e6d20dafba49d21c74c2f4f049a
Bhawesh02 <bhaweshagarwal02@gmail.com>
2022-08-09 19:41:05 +0530

    index for Hospital

--- static/JavaScript/Update_DataBase_Popup.js
+++ static/JavaScript/Update_DataBase_Popup.js
@@ -48,9 +48,49 @@ function Pop() {
     
 }
 
+
+
+function setCookie(cname, cvalue, exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    let expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  }
+  
+  function getCookie(cname) {
+    let name = cname + "=";
+    let ca = document.cookie.split(';');
+    for(let i = 0; i < ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 var ConfirmSubmitBtn = document.getElementById("ConfirmSubmit");
 ConfirmSubmitBtn.addEventListener("click",()=>{
-    const dict_values = {ListOfOrganUpdated} //Pass the javascript variables to a dictionary.
+    setCookie("User",0,1);
+    let HospitalIndex = getCookie("User");
+    const dict_values = {ListOfOrganUpdated,HospitalIndex} //Pass the javascript variables to a dictionary.
     const s = JSON.stringify(dict_values); // Stringify converts a JavaScript object or value to a JSON string
     $.ajax({
         url:"/UpdateDatabase",
