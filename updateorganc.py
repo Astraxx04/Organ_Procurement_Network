b1387fc310b2a31ebc804eb81c3d5c682f3bad67
Astraxx04 <gagan200254@gmail.com>
2022-08-10 00:06:58 +0530

    Organ update

--- updateorganc.py
+++ updateorganc.py
@@ -1,19 +1,29 @@
+from tkinter import FALSE
 import pandas as pd
+import csv as csv
 from csv import writer
+
 
 def updateorganc(organ, hsp):
     hosp=pd.read_csv('static/Database/hospitals.csv')
-    if(organ=='cornea'):ind=4
-    if(organ=='heaart'):ind=5
-    if(organ=='kidney'):ind=6
-    if(organ=='liver'):ind=7
-    if(organ=='lung'):ind=8
-    if(organ=='pancreas'):ind=9
 
-
-    for i in range (0, hosp.shape[0]):
-        if(hsp==hosp.iloc[i, 0]):
-            hosp.iloc[i, ind]=hosp.iloc[i, ind]+1
-            print(hosp.iloc[i, ind])
-
-
+    if(organ=='cornea'):
+        ind=4
+    if(organ=='heart'):
+        ind=5
+    if(organ=='kidney'):
+        ind=6
+    if(organ=='liver'):
+        ind=7
+    if(organ=='lung'):
+        ind=8
+    if(organ=='pancreas'):
+        ind=9
+    
+    print(hosp.iloc[hsp, ind])
+    hosp.iloc[hsp, ind]=hosp.iloc[hsp, ind]+1
+    print(hosp.iloc[hsp, ind])
+    
+    # writing into the file
+    hosp.to_csv("static/Database/hospitals.csv", index=False,mode='w')
+    
